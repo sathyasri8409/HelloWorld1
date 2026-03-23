b06c2850a3e97ce8334f1e5c95c8d541e2518765
sathyasri8409 <sathyasri8409@gmail.com>
2026-03-23 22:11:32 +0530

    Add files via upload

---
+++ java program/Helloworld1.java
@@ -0,0 +1,6 @@
+// File: Helloworld1.java
+public class Helloworld1 {
+   public static void main(String[] args) {
+       System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+   }
+}
